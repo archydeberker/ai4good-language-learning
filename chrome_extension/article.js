55f147ddeb1d145e29f8a114b9fe4c6fa11caa0d
Frederic Bastien <fbastien@nvidia.com>
2019-06-09 11:05:01 -0400

    Change the title every 3s.

--- chrome_extension/article.js
+++ chrome_extension/article.js
@@ -1,76 +1,86 @@
 var learning_enabled = true;
 var host = window.location.host;
 max_level = ["Easy"];
+bienvenues_text = ["Bienvenue", "Welcome", "Ahlan wa sahlan", "Bienvenido", "Danke", "Grazie", "Obrigado", "Cпасибо"];
 var score = {
   "Easy": 0,
   "Medium": 0,
   "Hard": 0
 };
+var current_bienvenue = 0;
+const bienvenue_header = document.createElement("div");
 
-if (learning_enabled){
+function display() {
+    if (learning_enabled){
 
-  // Bienvenue header div creation
-  const bienvenue_header = document.createElement("div");
-  bienvenue_header.textContent = "Bienvenue 🤗";
-  bienvenue_header.setAttribute("style", "background-color:#42a4f4; color:white; font-weight:bold; padding:5px;");
-  document.body.insertBefore(bienvenue_header, document.body.firstChild);
+	// Bienvenue header div creation
+	var str = bienvenues_text[current_bienvenue] + " ��";
+	bienvenue_header.textContent = bienvenues_text[current_bienvenue] + " 🤗";
+	bienvenue_header.setAttribute("style", "background-color:#42a4f4; color:white; font-weight:bold; padding:5px;");
+	document.body.insertBefore(bienvenue_header, document.body.firstChild);
+	current_bienvenue += 1;
+	current_bienvenue = current_bienvenue % bienvenues_text.length;
 
-  // Initial quiz text
-  const head_text = document.createElement("span");
-  head_text.textContent = "How familiar are you with this word ? ";
-  head_text.setAttribute("style", "padding-left:25%; font-style:normal;");
-  bienvenue_header.append(head_text);
+	// Initial quiz text
+	const head_text = document.createElement("span");
+	head_text.textContent = "How familiar are you with this word ? ";
+	head_text.setAttribute("style", "padding-left:5cm; font-style:normal;");
+	bienvenue_header.append(head_text);
 
-  // Assessement quizz span
-  var assessment_quiz_span = document.createElement("SPAN");
-  assessment_quiz_span.id = "assessment_quiz_span";
-  assessment_quiz_span.setAttribute("style", "padding-left:40px; padding-right:40px; text-transform:capitalize; font-style:italic;");
-  bienvenue_header.append(assessment_quiz_span);
+	// Assessement quizz span
+	var assessment_quiz_span = document.createElement("SPAN");
+	assessment_quiz_span.id = "assessment_quiz_span";
+	assessment_quiz_span.setAttribute("style", "padding-left:40px; padding-right:40px; text-transform:capitalize; font-style:italic;");
+	bienvenue_header.append(assessment_quiz_span);
 
-  // Assessement yes button
-  var bienvenue_yes = document.createElement("SPAN");
-  bienvenue_yes.id = "bienvenue_yes";
-  bienvenue_yes.setAttribute("style", "cursor: pointer;");
-  bienvenue_yes.textContent = "👍";
-  bienvenue_header.append(bienvenue_yes);
+	// Assessement yes button
+	var bienvenue_yes = document.createElement("SPAN");
+	bienvenue_yes.id = "bienvenue_yes";
+	bienvenue_yes.setAttribute("style", "cursor: pointer;");
+	bienvenue_yes.textContent = "👍";
+	bienvenue_header.append(bienvenue_yes);
 
-  // Empty divider span
-  var divider_span = document.createElement("SPAN");
-  divider_span.setAttribute("style", "padding-left:20px;");
-  bienvenue_header.append(divider_span);
+	// Empty divider span
+	var divider_span = document.createElement("SPAN");
+	divider_span.setAttribute("style", "padding-left:20px;");
+	bienvenue_header.append(divider_span);
 
-  // Assessement no button
-  var bienvenue_no = document.createElement("SPAN");
-  bienvenue_no.id = "bienvenue_no";
-  bienvenue_no.setAttribute("style", "cursor: pointer;");
-  bienvenue_no.textContent = "👎";
-  bienvenue_header.append(bienvenue_no);
+	// Assessement no button
+	var bienvenue_no = document.createElement("SPAN");
+	bienvenue_no.id = "bienvenue_no";
+	bienvenue_no.setAttribute("style", "cursor: pointer;");
+	bienvenue_no.textContent = "👎";
+	bienvenue_header.append(bienvenue_no);
 
-  // Actual assessement quizz
-  var assessment_quiz_dict = [{"bonjour": "easy"},{"ecole": "medium"},{"marche": "hard"},{"dehors": "easy"}];
+	// Actual assessement quizz
+	var assessment_quiz_dict = [{"bonjour": "easy"},{"ecole": "medium"},{"marche": "hard"},{"dehors": "easy"}];
 
-  word_count = 0;
+	word_count = 0;
 
-  function getMaxScore() {
-    max_score = 0;
-    max_level = ["Easy"];
-    for (var level in score) {
-      if (score[level] > max_score) {
-        max_score = score[level];
-        max_level = level;
-      }
-    }
-  }
+	function getMaxScore() {
+	    max_score = 0;
+	    max_level = ["Easy"];
+	    for (var level in score) {
+		if (score[level] > max_score) {
+		    max_score = score[level];
+		    max_level = level;
+		}
+	    }
+	}
 
-  assessment_quiz_span.innerText = Object.keys(assessment_quiz_dict[word_count]);
-  console.log(Object.values(assessment_quiz_dict[word_count]))
+	assessment_quiz_span.innerText = Object.keys(assessment_quiz_dict[word_count]);
+	console.log(Object.values(assessment_quiz_dict[word_count]))
 
-  // Level counter
-  var assessment_quiz_level_span = document.createElement("SPAN");
-  assessment_quiz_level_span.textContent = "Level: " + max_level;
-  assessment_quiz_level_span.setAttribute("style", "float:right;");
-  bienvenue_header.append(assessment_quiz_level_span);
-}
+	// Level counter
+	var assessment_quiz_level_span = document.createElement("SPAN");
+	assessment_quiz_level_span.textContent = "Level: " + max_level;
+	assessment_quiz_level_span.setAttribute("style", "float:right;");
+	bienvenue_header.append(assessment_quiz_level_span);
+    }//end if
+    setTimeout(display, 3000);
+}//end display function
+
+display();
 
 if (host == "www.nytimes.com"){
   text_chunks = document.getElementsByClassName("g-body");
