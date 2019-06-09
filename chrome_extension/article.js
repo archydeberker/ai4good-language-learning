8dc3772abffbefb7046b45cf22d91c3c4b76b9b1
jadfegh <jad4feghali@gmail.com>
2019-06-09 11:52:07 -0400

    parsing sring

--- chrome_extension/article.js
+++ chrome_extension/article.js
@@ -7,6 +7,17 @@ var score = {
   "Hard": 0
 };
 
+function getMaxScore() {
+  max_score = 0;
+  max_level = ["Easy"];
+  for (var level in score) {
+    if (score[level] > max_score) {
+      max_score = score[level];
+      max_level = level;
+    }
+  }
+}
+
 if (learning_enabled){
 
   // Bienvenue header div creation
@@ -30,7 +41,6 @@ if (learning_enabled){
   // Assessement yes button
   var bienvenue_yes = document.createElement("SPAN");
   bienvenue_yes.id = "bienvenue_yes";
-  bienvenue_yes.setAttribute("style", "cursor: pointer;");
   bienvenue_yes.textContent = "👍";
   bienvenue_header.append(bienvenue_yes);
 
@@ -42,7 +52,6 @@ if (learning_enabled){
   // Assessement no button
   var bienvenue_no = document.createElement("SPAN");
   bienvenue_no.id = "bienvenue_no";
-  bienvenue_no.setAttribute("style", "cursor: pointer;");
   bienvenue_no.textContent = "👎";
   bienvenue_header.append(bienvenue_no);
 
@@ -50,49 +59,54 @@ if (learning_enabled){
   var assessment_quiz_dict = [{"bonjour": "easy"},{"ecole": "medium"},{"marche": "hard"},{"dehors": "easy"}];
 
   word_count = 0;
-
-  function getMaxScore() {
-    max_score = 0;
-    max_level = ["Easy"];
-    for (var level in score) {
-      if (score[level] > max_score) {
-        max_score = score[level];
-        max_level = level;
-      }
-    }
-  }
+  getMaxScore()
 
   assessment_quiz_span.innerText = Object.keys(assessment_quiz_dict[word_count]);
+
+  to_do_onclick = 'score[' + Object.values(assessment_quiz_dict[word_count]) + '++];'
+  bienvenue_yes.setAttribute('onclick', to_do_onclick)
   console.log(Object.values(assessment_quiz_dict[word_count]))
 
-  // Level counter
+  // Level
   var assessment_quiz_level_span = document.createElement("SPAN");
   assessment_quiz_level_span.textContent = "Level: " + max_level;
   assessment_quiz_level_span.setAttribute("style", "float:right;");
   bienvenue_header.append(assessment_quiz_level_span);
 }
 
-if (host == "www.nytimes.com"){
-  text_chunks = document.getElementsByClassName("g-body");
-  for (let index = 0; index < text_chunks.length; index++) {
-    var element = text_chunks[index].innerText;
-    console.log(element);
+// if (host == "www.nytimes.com"){
+//   text_chunks = document.getElementsByClassName("p");
+//   for (let index = 0; index < text_chunks.length; index++) {
+//     var element = text_chunks[index].innerText;
+//     console.log(element);
 
-    const Http = new XMLHttpRequest();
-    const url = "http://662f13f9.ngrok.io/query-example?text=" + element;
-    Http.open("GET", url);
-    Http.send();
+//     const Http = new XMLHttpRequest();
+//     const url = "http://662f13f9.ngrok.io/query-example?text=" + element;
+//     Http.open("GET", url);
+//     Http.send();
 
-    Http.onreadystatechange = (e) => {
-      console.log(Http.responseText)
-    }
+//     Http.onreadystatechange = (e) => {
+//       obj = JSON.parse(Http.responseText);
+//       console.log(obj[0]);
+//       obj = JSON.parse(json);
+//       for (let index = 2; index < obj.length; index++) {
+//         console.log(obj[index]);
+//       //   const element = array[index];
+//       //   if(element.text)
+//       //   {
+//       //     console.log(element.original) 
+//       //   }
+//       //   else console.log(element.text);
+//       // }
+//     }
 
-    //text_chunks[index].innerText = 'hello hello hello hello';
-  }
-}
+//     //text_chunks[index].innerText = 'hello hello hello hello';
+//   }
+// }
 
-if (host == "www.ledevoir.com" || host == "time.com"){
+if (host == "www.ledevoir.com" || host == "time.com" || host == "en.wikipedia.org" || host == ""){
   text_chunks = document.getElementsByTagName('p');
+  console.log(text_chunks);
   for (let index = 0; index < text_chunks.length; index++) {
     var element = text_chunks[index].innerText;
     console.log(element);
@@ -106,10 +120,20 @@ if (host == "www.ledevoir.com" || host == "time.com"){
 
     Http.onreadystatechange = function () {
       if(Http.readyState === 4 && Http.status === 200) {
-        console.log(Http.responseText);
-        text_resampled = Http.responseText;
+        obj = JSON.parse(Http.responseText);
+        console.log(obj[0]);
+        for (let index = 2; index < obj.length; index++) {
+          console.log(obj[index]);
+          // const element = array[index];
+          // if(element.text)
+          // {
+          //   console.log(element.original) 
+          // }
+          // else console.log(element.text);
+        }
       }
-    };
+    }
+  }
 
     // text_chunks[index].innerText ="";
     // for (var word in text_resampled) {
@@ -118,5 +142,4 @@ if (host == "www.ledevoir.com" || host == "time.com"){
     //   // text_chunks[index].innerText += text_resampled[word];
     //   // console.log( word + "  " + text_resampled[word])
     // }
-  }
 }
